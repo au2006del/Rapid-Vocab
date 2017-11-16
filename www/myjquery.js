2ffaa82f2ab27ff11660b58ae6770da4f5fe5cab
Anshul upadhyay <[au2006del@gmail.com]>
2017-11-15 20:33:05 -0500

    project done

---
+++ www/myjquery.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+
+    $("#bt1").click(function() {
+        alert("Wrong Answer. Right answer is Abnormal.");
+    });
+    $("#bt2").click(function() {
+        alert("Wrong Answer. Right answer is Abnormal.");
+    });
+    $("#bt3").click(function() {
+        alert("Right Answer");
+    });
+
+    $("#baf1").click(function() {
+        alert("Right Answer");
+    });
+    $("#baf2").click(function() {
+        alert("Wrong Answer. Right answer is Puzzle.");
+    });
+    $("#baf3").click(function() {
+        alert("Wrong Answer. Right answer is Puzzle.");
+    });
+
+
+    $("#but1").click(function() {
+        alert("Wrong Answer. Right answer is Support.");
+    });
+    $("#but2").click(function() {
+        alert("Right Answer");
+    });
+    $("#but3").click(function() {
+        alert("Wrong Answer. Right answer is Support.");
+    });
+
+    $("#ca1").click(function() {
+        alert("Right Answer");
+    });
+    $("#ca2").click(function() {
+        alert("Wrong Answer. Right answer is Harsh Sounds.");
+    });
+    $("#ca3").click(function() {
+        alert("Wrong Answer. Right answer is Harsh Sounds.");
+    });
+
+    $("#cal1").click(function() {
+        alert("Right Answer");
+    });
+    $("#cal2").click(function() {
+        alert("Wrong Answer. Right answer is Ability quality.");
+    });
+    $("#cal3").click(function() {
+        alert("Wrong Answer. Right answer is Ability quality.");
+    });
+
+    $("#da1").click(function() {
+        alert("Right Answer");
+    });
+    $("#da2").click(function() {
+        alert("Wrong Answer. Right answer is Delicately beautiful.");
+    });
+    $("#da3").click(function() {
+        alert("Wrong Answer. Right answer is Delicately beautiful.");
+    });
+
+});
+
+
+
+
+
